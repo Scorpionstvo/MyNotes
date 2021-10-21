bde5b718db7bd6a6550dacc714d7c87eb6a94fa2
Olesya <garipolesya@mail.ru>
2021-10-21 16:13:24 +0300

    FRAGMENTS: Adding fun onBackPressedListener, which determines whether to close the fragment or affect the view. Remove interface OnBackPressedListener.

--- app/src/main/java/com/example/myproject/project/hidden/HiddenNotesFragment.kt
+++ app/src/main/java/com/example/myproject/project/hidden/HiddenNotesFragment.kt
@@ -6,9 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.myproject.project.util.OnBackPressedListener
 import com.example.currentnote.R
 import com.example.currentnote.databinding.FragmentHiddenNotesBinding
 import com.example.myproject.project.type.Type
@@ -21,8 +21,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlin.collections.ArrayList
 
-class HiddenNotesFragment : Fragment(), HiddenNoteAdapter.ShowDetailListener,
-    OnBackPressedListener {
+class HiddenNotesFragment : Fragment(), HiddenNoteAdapter.ShowDetailListener {
     private var binding: FragmentHiddenNotesBinding? = null
     private val adapter = HiddenNoteAdapter(this)
     private val dbManager = MyApplication.dbManager
@@ -61,6 +60,7 @@ class HiddenNotesFragment : Fragment(), HiddenNoteAdapter.ShowDetailListener,
         initToolbar()
         initSearchView()
         initBottomNavigationView()
+        initOnBackPressedListener()
     }
 
     private fun recyclerViewStateCreated() {
@@ -260,18 +260,22 @@ class HiddenNotesFragment : Fragment(), HiddenNoteAdapter.ShowDetailListener,
 
     }
 
-    override fun onBackPressed(): Boolean {
-        return if (binding?.btMenuHiddenNotes?.visibility == View.VISIBLE) {
-            binding?.btMenuHiddenNotes?.visibility = View.GONE
-            binding?.tvHiddenNotesTitle?.text =
-                resources.getString(R.string.title_toolbar_hidden_notes)
-            adapter.isShowCheckBox(false)
-            binding?.tbHiddenNotes?.menu?.clear()
-            binding?.tbHiddenNotes?.inflateMenu(R.menu.hidden_toolbar_menu)
-            false
-        } else true
+    private fun initOnBackPressedListener() {
+        activity?.onBackPressedDispatcher?.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding?.btMenuHiddenNotes?.visibility == View.VISIBLE) {
+                    binding?.btMenuHiddenNotes?.visibility = View.GONE
+                    binding?.tvHiddenNotesTitle?.text =
+                        resources.getString(R.string.title_toolbar_hidden_notes)
+                    adapter.isShowCheckBox(false)
+                    binding?.tbHiddenNotes?.menu?.clear()
+                    binding?.tbHiddenNotes?.inflateMenu(R.menu.hidden_toolbar_menu)
+                } else {
+                    isEnabled = false
+                    activity?.onBackPressed()
+                }
+            }
+        })
     }
 
 }
-
-
